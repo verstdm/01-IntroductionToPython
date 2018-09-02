4f61b48b95fb2104ce73f9e2c98d658938dd12d8
verstdm <verstdm@rose-hulman.edu>
2018-09-02 12:16:18 -0400

    Done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Dylsn Verst.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -27,3 +27,20 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+i = rg.SimpleTurtle()
+i.pen = rg.Pen('midnight blue', 4)
+j = rg.SimpleTurtle()
+j.pen = rg.Pen('maroon', 4)
+i.speed = 1000
+j.speed = 1000
+size = 288
+for k in range(72):
+    i.draw_square(size)
+    size -= 2
+    j.draw_circle(size/2)
+    size -= 2
+    i.right(5)
+    j.right(5)
+window.close_on_mouse_click()
